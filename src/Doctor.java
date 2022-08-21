830304f4cb4e6e0f483081cd5f61da2da8b24e2d
Àlex Grau Roca <alexgrauroca91@gmail.com>
2022-08-21 10:50:30 +0200

    class 5

---
+++ src/Doctor.java
@@ -0,0 +1,12 @@
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+
+    /**
+     * Print the doctor name
+     */
+    public void showName() {
+        System.out.println(name);
+    }
+}
