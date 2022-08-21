7e071ca934aea769eaa5c66d3fa1918138e94425
Àlex Grau Roca <alexgrauroca91@gmail.com>
2022-08-21 10:56:04 +0200

    class 6

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,7 +1,35 @@
+/**
+ * Doctor object
+ */
 public class Doctor {
+    /**
+     * Doctor id
+     */
     int id;
+    /**
+     * Doctor name
+     */
     String name;
+    /**
+     * Doctor speciality
+     */
     String speciality;
+
+    /**
+     * New doctor instance
+     */
+    Doctor() {
+        System.out.println("Building a doctor object");
+    }
+
+    /**
+     * New doctor instance
+     *
+     * @param name
+     */
+    Doctor(String name) {
+        System.out.println("The doctor name is: " + name);
+    }
 
     /**
      * Print the doctor name
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Doctorito Doctorando";
+        Doctor myDoctor = new Doctor("Doctorito Doctorando");
+        //myDoctor.name = "Doctorito Doctorando";
         myDoctor.showName();
     }
 }
